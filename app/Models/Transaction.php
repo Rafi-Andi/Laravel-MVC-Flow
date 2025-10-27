ead39e40783b24a0d57888c625a8592f5216ed62
Rafi-Andi <rafiandiprayitno9528@gmail.com>
2025-10-27 11:18:50 +0700

    DB Seeder and Faker

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Transaction extends Model
 {
-    //
+    public function user():BelongsTo{
+        return $this->belongsTo(User::class);
+    }
+
+    public function transaction_details():HasMany{
+        return $this->hasMany(TransactionDetail::class);
+    }
 }
--- app/Models/TransactionDetail.php
+++ app/Models/TransactionDetail.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class TransactionDetail extends Model
 {
-    //
+    public function transaction():BelongsTo{
+        return $this->belongsTo(Transaction::class);
+    }
+
+    public function item():BelongsTo{
+        return $this->belongsTo(Item::class);
+    }
 }
